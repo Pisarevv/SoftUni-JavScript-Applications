883eb7421c1a22adec4399349279f84db05183bc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-11-24 23:29:46 +0200

    Request api created

---
+++ 8.Modular Applications - Exercise/01.Team-Manager/src/api/api.js
@@ -0,0 +1,70 @@
+import { getAccessToken, deleteUser } from "../util.js";
+
+let target = 'http://localhost:3030';
+
+async function request(url,method,data){
+    let options = {
+        method,
+        headers : {}
+    }
+
+    let token = getAccessToken();
+
+    if(token){
+        options.headers['X-Authorization'] = token;
+    }
+
+    if(data) {
+       options.headers['Content-type'] = "application/json";
+       options.body = JSON.stringify(data);
+    }
+
+    try{
+        let result = await fetch(target + url,options);
+
+        if(result.ok != true){
+
+            if(result == 403){
+                deleteUser();
+            }
+         throw new Error(await result.json());
+        
+        }
+        if(result.status == 204){
+            return result;
+        }
+        else{
+            return result.json();
+        }
+        
+    }
+    catch(error){
+        alert(error.message);
+        throw new Error();
+    }
+
+}
+
+
+export async function get(url){
+    return request(url,'get');
+}
+
+export async function post(url,data){
+    return request(url,'post',data);
+}
+
+export async function del(url){
+    return request(url,'del');
+}
+
+export async function put(url){
+    return request(url,'put',data);
+}
+
+export {
+    get,
+    post,
+    put,
+    del as delete
+}
