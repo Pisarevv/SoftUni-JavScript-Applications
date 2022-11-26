a8b2c7a8db6240fca0ea9297fb815b4b48d29d41
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-11-26 19:54:45 +0200

    Added: catalog view, create view, details view Functionality : manipulation on added views

--- 9.Exam preparation/Pet care/src/app.js
+++ 9.Exam preparation/Pet care/src/app.js
@@ -3,6 +3,9 @@ import {page} from "./lib.js"
 import { updateNav } from "./middleware/navbar.js";
 import { decorateContext } from "./middleware/render.js";
 import { setSession } from "./middleware/sesison.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
 import { showHome } from "./views/home.js";
 import { showLogin } from "./views/login.js";
 import { showRegister } from "./views/register.js";
@@ -15,6 +18,9 @@ page(decorateContext);
 page('/', showHome);
 page('/login', showLogin);
 page('/register', showRegister);
+page('/catalog', showCatalog);
+page('/catalog/:id',showDetails)
+page('/create', showCreate)
 
 
 let logoutBtn = document.getElementById("logoutBtn");
